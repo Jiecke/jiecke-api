886726e5c960043f0d20500f49796ede8b0de5f5
mouweng <325242850@qq.com>
2022-03-22 14:30:16 +0800

    Mod version

---
+++ server.py
@@ -0,0 +1,36 @@
+import flask
+from template import Template
+from flask import request, jsonify, make_response
+import time
+import os
+
+photo_dir = "./photo/"
+server = flask.Flask(__name__)
+
+@server.route('/ping', methods=['get'])
+def ping():
+    return jsonify({"code": "1", "msg": "success", "data": "pong"})
+
+
+@server.route('/', methods=['post'])
+def convert():
+    upload_image = request.files.get('image')
+    type = request.form.get('type')
+    print(type)
+    if upload_image is None:
+        return jsonify({"code": "-1", "msg" : "图片上传失败，请重新上传!", "data" : ""})
+
+    upload_path = os.path.join(photo_dir, str(int(time.time())))
+    if not os.path.exists(upload_path):
+        os.makedirs(upload_path)
+    file_path = os.path.join(upload_path, upload_image.filename)
+    upload_image.save(file_path)
+    t = Template(type, file_path)
+    res_img_path = t.synthetic()
+    image_data = open(res_img_path, "rb").read()
+    response = make_response(image_data)
+    response.headers['Content-Type'] = 'image/png'
+    return response
+
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', port=8080, debug=True)
